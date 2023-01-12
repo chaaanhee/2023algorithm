8c2d504ac892d9708ddbb878791e63575e7f3a11
chaaanhee <cksgml8157@gmail.com>
2023-01-12 14:05:05 +0900

    2018

--- doitCote/src/baekjoon/연속된자연수의합구하기.java
+++ doitCote/src/baekjoon/연속된자연수의합구하기.java
@@ -15,7 +15,7 @@ public class 연속된자연수의합구하기 {
 		int count = 1;
 		int start_index = 1;
 		int end_index = 1;
-		// 책은 sum = 1로 지정한 이유..
+		// 책은 sum = 1로 지정한 이유..?
 		int sum = 0;
 		
 		while(end_index != N) {
@@ -27,6 +27,7 @@ public class 연속된자연수의합구하기 {
 				sum = sum + end_index;
 				count++;
 				end_index++;
+				// 내가 생각하기에는 start_index가 이동하는게 마지막에 들어가는게 맞는 것 같다.
 			} else if(sum > N) {
 				sum = sum - start_index;
 				start_index++;
